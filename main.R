2596fdad70a51d33054ce6834840a5a851daf2d6
RsoltanGit <reza.soltanpoor@rmit.edu.au>
2019-09-15 17:08:08 +1000

    performing some code refactoring on main.R file.

--- main.R
+++ main.R
@@ -5,8 +5,7 @@ rm(list=ls())
 # setting the working directory
 # setwd(Sys.getenv("R_USER"))
 
-
-##### ====== Setting up the environment (installing packages and loading libraries) ====== #####
+## Setting up the environment (installing packages and loading libraries) ##
 ## list of packages needed
 # listOfPackages_needed <- c("tm", "tidyverse", "quanteda", "qdap", "superml", "RJSONIO", "plumber")
 ## checking whether or not the required packages have already been installed.
@@ -39,7 +38,7 @@ library(quanteda)
 #library(superml)
 library(RJSONIO)
 
-##### ====== user-defined functions ====== #####
+# the main function which gets the query from the API and generates the JSON list of matched topics.
 keywordAssoc = function(query) {
   # create the toSpace content transformer to replace the passed "x" with a space
   toSpace <- content_transformer(function(x, pattern) {return (gsub(pattern, " ", x))})
@@ -49,34 +48,32 @@ keywordAssoc = function(query) {
     weightTfIdf(x, normalize = T)
   }
   
-  ##### ======  ====== #####
-  ### In Vector Space Model: Rank by the similarity of each document to the query and you have a search engine.
-  
+  ## In Vector Space Model: Rank by the similarity of each document to the query and you have a search engine.
   # reading the tags from an external data-set.
   tags = read.csv("./datasets/tag_sets.csv", header = FALSE, stringsAsFactors = FALSE)
   names(tags) = "tags"
   tgs = tags$tags
-  # sample 30,000 tags
-  tgs_1 = tgs[1:30000]
+  # the following code snippet samples 10k tags out of 90k (it works with 30k as well).
+  # the reason is efficiency matters, I will find a way to address the whole dataset (it works with 30k as well, but for the sake of efficiency, I limited that to 10k).
+  # sample 10,000 tags
+  tgs_1 = tgs[1:10000]
   number_of_tags <- length(tgs_1)
+  # just giving each tag a name (tag + an auto generated number)
   names(tgs_1) <- paste0("tag", c(1:number_of_tags))
-  # the query
+  # the query read from the API
   if (query == "") {
     q <- ""
   } else {
     q <- query
   }
-  # q = query
-  # q <- "game"
-  # q <- "cute cat"
-  # q = "baby"
-  # vector source
+  # vector source creation (given the tags at hand and the query read from the API request)
   docs = VectorSource(c(tgs_1, q))
+  # adding the query's name
   docs$Names = c(names(tgs_1), "query")
   # a corpus is a collection of texts, often with extensive manual annotation.
   crps = Corpus(docs)
   crps
-  # pre-processing & standardization
+  ## pre-processing & standardization
   # replacing commas with space
   crps = tm_map(crps, toSpace, ",")
   # remove punctuations, numbers and any extra white space & make the words lower-case
@@ -87,17 +84,12 @@ keywordAssoc = function(query) {
   # stemming
   crps = tm_map(crps, stemDocument)
   crps[[1]]$content
-  # building vector space model
+  ## building vector space model
   tdm = TermDocumentMatrix(crps)
   inspect(tdm)
+  # creating the matrix out of the tdm. This process encounters efficiency issue when passing 90k tags!
   tdm_matrix = as.matrix(tdm)
   # tf.idf weight of \( (1 + \log_2(tf)) \times \log_2(N/df) \) will be calculated...
-  # Note that whenever a term does not occur in a specific document, or when it appears in every document, its weight is zero.
-  # N.docs = the number of docs
-  # tfidf_m <- t(apply(tdm_3_m, c(1), FUN = get.weights.per.term.vec))
-  # colnames(tfidf_m) <- colnames(tdm_3_m)
-  # tfidf_m[0:3, 1:5]
-  # tdm_3.1 = TermDocumentMatrix(crps, control = list(weighting = weightTfIdf))
   # the term doc matrix with the tf.idf weights calculated for each cell
   tdm_w_tfidf = TermDocumentMatrix(crps, control = list(weighting = w_tfidf))
   # inspecting the content of the created term-doc-matrix
@@ -115,7 +107,6 @@ keywordAssoc = function(query) {
   # This will return values of "cos(Theta)" for each document vector and the query vector.
   scores = t(q_vec) %*% tdm_w_tfidf_m
   # With scores in hand, we can rank the documents by their cosine similarities with the query vector.
-  # res = data.frame(tag = names(tgs_1), score = t(scores), text = tgs_1)
   res = data.frame(score = t(scores), text = tgs_1)
   # just taking into consideration those whose scores are not ZERO (match to some extent)!
   res = res[res$score > 0, ]
@@ -125,30 +116,16 @@ keywordAssoc = function(query) {
   options(width = 2000)
   print(res, right = FALSE, digits = 2, row.names = FALSE)
   
-  
-  ## furthermore normalize each column vector in our tfidf matrix so that its norm is one.
-  # tfidf.matrix <- scale(tfidf.matrix, center = FALSE, scale = sqrt(colSums(tfidf.matrix^2)))
-  # tfidf.matrix[0:3, ]
-  
-  
-  ####### top score (max)
+  ## top score(s) (max) calculation
   top_scored_tags = res[res$score == max(res$score), ]
   # change the data type of the "text" col from factor to char (String)
   top_scored_tags$text = as.character(top_scored_tags$text)
   
-  ###### generating the JSON output
+  ## generating the JSON output
   ## TODO: check to merge (concatenate) all TEXT columns (not the 1st top one) satisfying the MAX condition above...
   t1 = top_scored_tags$text
-  # names(t1) = "related_topics"
-  # t1_json = list(key = "related_topics", values = t1)
-  # cat(toJSON(list(related_topics = t1)))
-  # 
+  # splitting the output text based on commas they have to form the JSON list of values.
   t2 = c(unlist(strsplit(t1, split = ",")))
-  #t3 = cat(toJSON(list(related_topics = t2)))  
-  #list(related_topics = t2)
+  # returning the result list (later will be illustrated as JSON list as the RESTful API's response).
   return(t2)
 }
-
-#result = keywordAssoc("geek")
-#result
-
